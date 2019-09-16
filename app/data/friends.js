153aecd82c9dea345ee2aac1c67129a15bd546dd
Jose Borunda <jose.h.borunda@gmail.com>
2019-09-15 21:05:56 -0600

    added friends, API list is working on Homepage, api routes and html are talking to each other

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+var friends = [
+	{
+		name: "Michel Gary Scott",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest?cb=20170701090332",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "James Duncan Halpert",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg/revision/latest?cb=20170701084550",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Dwight Kurt Schrute III",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Dwight_.jpg/revision/latest?cb=20170701082424",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Pamela Morgan Halpert",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesley.jpg/revision/latest?cb=20170701084358",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Andrew Baines Bernard",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/b/b5/Andy_Bernard.jpg/revision/latest?cb=20170701084205",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+		name: "Angela Noelle Schrute",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/0b/Angela_Martin.jpg/revision/latest?cb=20170701090232",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+		name: "Kelly Rajnigandha Kapoor",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/6/69/Kelly_Kapoor.jpg/revision/latest?cb=20170701090501",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+		name: "Kevin Jaye Malone",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/b/b2/2009Kevincropped.PNG/revision/latest?cb=20170701083657",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+		name: "Stanley James Hudson",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/2/23/Stanley_Hudson.jpg/revision/latest?cb=20170701085445",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,1 +1,39 @@
+// Dependencies
+// =============================================================
 var express = require("express");
+var path = require("path");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//get routings:
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
