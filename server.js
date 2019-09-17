c978693812f05ce046327bffff7243e0ad509485
Jose Borunda <jose.h.borunda@gmail.com>
2019-09-16 22:22:52 -0600

    app is talking to each other, but not displaying modal

--- server.js
+++ server.js
@@ -2,35 +2,21 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//get routings:
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
-
-
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
